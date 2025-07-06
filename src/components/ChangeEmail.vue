79b1b79b3ea7d1a84d52284644c1e79745dd6a20
ncantona <nico.cantonati@outlook.com>
2025-07-06 19:09:25 +0200

    refactored change pw and change email with custom components

--- src/components/ChangeEmail.vue
+++ src/components/ChangeEmail.vue
@@ -1,28 +1,18 @@
 <script setup>
-    import DefaultButton from './small/DefaultButton.vue';
+    import CustomInputText from './small/CustomInputText.vue';
+    import CustomButtonSubmit from './small/CustomButtonSubmit.vue';
 </script>
 
 <template>
     <div class="border-1 rounded-xl md:border-none p-4">
         <div class="flex flex-col md:flex-row justify-center gap-10">
-            <div>
-                <ul class="flex flex-col md:flex-row md:gap-5">
-                    <li class="flex flex-col gap-2 mb-3">
-                        <label for="newEmail">New Email*</label>
-                        <input type="text" id="newEmail" name="newEmail" class="border-1 rounded-sm outline-none p-2 focus-within:border-2 focus-within:border-blue-500">
-                    </li>
-                    <li class="flex flex-col gap-2 mb-3">
-                        <label for="confirmPassword">Confirm Your Password*</label>
-                        <input type="password" id="confirmPassword" name="confirmPassword" class="border-1 rounded-sm outline-none p-2 focus-within:border-2 focus-within:border-blue-500">
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="flex flex-row justify-center items-cente mt-5">
-            <DefaultButton class="p-2 bg-blue-600 text-white rounded-md hover:bg-blue-500"
-                @click="changeRoute">
-                Change Email
-            </DefaultButton>
+            <form class="flex flex-col justify-center items-center gap-5">
+                <div class="flex flex-col md:flex-row gap-5 w-full">
+                    <CustomInputText type="email" label="New Email*" name="newEmail"/>
+                    <CustomInputText type="password" label="Confirm Your Password*" name="password"/>
+                </div>
+                <CustomButtonSubmit @submit="sendBackEnd">Change Email</CustomButtonSubmit>
+            </form>
         </div>
     </div>
 </template>
--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -1,32 +1,19 @@
 <script setup>
-    import DefaultButton from './small/DefaultButton.vue';
+    import CustomInputText from './small/CustomInputText.vue';
+    import CustomButtonSubmit from './small/CustomButtonSubmit.vue';
 </script>
 
 <template>
     <div class="border-1 rounded-xl md:border-none p-4">
         <div class="flex flex-col md:flex-row justify-center gap-10">
-            <div>
-                <ul class="flex flex-col md:flex-row md:gap-5">
-                    <li class="flex flex-col gap-2 mb-3">
-                        <label for="currentPassword">Current Password*</label>
-                        <input type="password" id="currentPassword" name="currentPassword" class="border-1 rounded-sm outline-none p-2 focus-within:border-2 focus-within:border-blue-500">
-                    </li>
-                    <li class="flex flex-col gap-2 mb-3">
-                        <label for="newPassword">New Password*</label>
-                        <input type="password" id="newPassword" name="newPassword" class="border-1 rounded-sm outline-none p-2 focus-within:border-2 focus-within:border-blue-500">
-                    </li>
-                    <li class="flex flex-col gap-2 mb-3">
-                        <label for="retypePassword">Retype Password*</label>
-                        <input type="password" id="retypePassword" name="retypePassword" class="border-1 rounded-sm outline-none p-2 focus-within:border-2 focus-within:border-blue-500">
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="flex flex-row justify-center items-cente mt-5">
-            <DefaultButton class="p-2 bg-blue-600 text-white rounded-md hover:bg-blue-500"
-                @click="changeRoute">
-                Change Password
-            </DefaultButton>
+            <form class="flex flex-col justify-center items-center gap-5">
+                <div class="flex flex-col md:flex-row gap-5 w-full">
+                    <CustomInputText type="password" label="Current Password*" name="currentPassword"/>
+                    <CustomInputText type="password" label="New Password*" name="newPassword"/>
+                    <CustomInputText type="password" label="Retype Password*" name="retypePassword"/>
+                </div>
+                <CustomButtonSubmit @submit="sendBackEnd">Change Password</CustomButtonSubmit>
+            </form>
         </div>
     </div>
 </template>
