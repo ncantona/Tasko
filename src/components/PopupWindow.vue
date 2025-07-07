72cb4cdb29a1660f2f566aec98b3b6e29d15a616
ncantona <nico.cantonati@outlook.com>
2025-07-08 00:20:43 +0200

    added popup component

---
+++ src/components/PopupWindow.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import successIcon from '@/images/Success.svg'
+    import errorIcon from '@/images/Remove.svg'
+
+    const emit = defineEmits('close');
+    const props = defineProps({
+        type: {
+            type: String,
+            default: 'success'
+        }
+    });
+
+    setTimeout(() => {
+        emit('close');
+    }, 3000);
+
+</script>
+
+<template>
+    <div class="absolute top-2 flex flex-row justify-center items-center bg-white/50 shadow-xl gap-4 p-4 text-xl">
+        <img :src="type === 'success' ? successIcon : errorIcon" :alt="`${props.type} svg`" class="w-15 h-15">
+        <slot>Popup Message</slot>
+    </div>
+</template>
