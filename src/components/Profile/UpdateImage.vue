d106df7ffc02f4ab891e9311f9cd19de80d60159
ncantona <nico.cantonati@outlook.com>
2025-07-17 17:25:14 +0200

    deleted click listeners

--- src/components/Profile/UpdateImage.vue
+++ src/components/Profile/UpdateImage.vue
@@ -1,17 +1,17 @@
 <script setup>
     import DefaultButton from '@/components/small/DefaultButton.vue';
+
+    const pictureExists = false;
 </script>
 
 <template>
     <div class="flex flex-col justify-start md:w-2/10 items-center relative">
         <img src="@/images/DefaultProfilePicture.svg" alt="Profile Picture" class="w-5/6 rounded-full max-w-70 min-w-40">
         <div class="flex flex-row items-center mt-2">
-            <DefaultButton v-show="pictureExists"
-                @click="removePhoto">
+            <DefaultButton v-show="pictureExists">
                 <img src="@/images/Remove.svg" alt="Remove Button" class="w-4/6">
             </DefaultButton>
-            <DefaultButton class="text-blue-600 hover:text-blue-500"
-                @click="changePhoto">
+            <DefaultButton class="text-blue-600 hover:text-blue-500">
                 choose Photo
             </DefaultButton>
         </div>
