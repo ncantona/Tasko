0cb7e8b2187c560f5e22da5e3350e576f928cad6
ncantona <nico.cantonati@outlook.com>
2025-07-18 11:26:55 +0200

    added functionality so i can check the windowsize

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,34 @@
 <script setup>
     import CustomHeader from '@/components/small/CustomHeader.vue';
     import TasklistSmall from '@/components/TasklistsSmall/TasklistSmall.vue';
+    import { ref, onMounted, onUnmounted, computed } from 'vue';
+
+    const windowWidth = ref(window.innerWidth);
+    console.log(windowWidth);
+
+    const handleResize = () => {
+        windowWidth.value = window.innerWidth;
+    };
+
+    onMounted(() => {
+        window.addEventListener('resize', handleResize);
+    });
+
+    onUnmounted(() => {
+        window.removeEventListener('resize', handleResize);
+    });
+
+    const isMobile = computed(() => windowWidth.value < 768);
 </script>
 
 <template>
     <div>
         <CustomHeader>My Tasklists</CustomHeader>
-        <div class="flex flex-col justify-center md:hidden">
+        <div v-if="isMobile" class="flex flex-col justify-center ">
             <TasklistSmall class="mt-5"/>
+        </div>
+        <div v-if="!isMobile">
+            
         </div>
     </div>
 </template>
